8ab1852566d3cf28c193bbe661bf30b64fa19993
Project <projectinsurance4@gmail.com>
2019-05-14 10:53:36 +0530

    i added Addition

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -10,7 +10,9 @@ namespace Sample
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a + b);
         }
     }
 }
